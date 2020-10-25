08d1c4847e629ae9bc1554bf874935c5172d8d34
JRK <jrkn87@gmail.com>
2020-10-25 17:32:20 +0100

    @DisplayName and @Disabled

--- src/test/java/pl/jrkn87/junit/CartTest.java
+++ src/test/java/pl/jrkn87/junit/CartTest.java
@@ -1,13 +1,18 @@
 package pl.jrkn87.junit;
 
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayName("Cart test")
 class CartTest {
 
+    @Disabled
+    @DisplayName("Simulate large order")
     @Test
     void simulateLargeOrder() {
         //given
