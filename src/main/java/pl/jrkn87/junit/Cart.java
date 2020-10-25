3a646c24ae817f4b6d6269298646f48c1b047509
JRK <jrkn87@gmail.com>
2020-10-25 17:08:14 +0100

    assertTimeout()

---
+++ src/main/java/pl/jrkn87/junit/Cart.java
@@ -0,0 +1,27 @@
+package pl.jrkn87.junit;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cart {
+    private List<Order> orders = new ArrayList<>(0);
+
+    void addOrderToCart(Order order) {
+        this.orders.add(order);
+    }
+
+    void clearCart() {
+        orders.clear();
+    }
+
+    void simulateLargeOrder() {
+        for (int i = 0; i < 1_000; i++) {
+            Meal meal = new Meal(i%10, "Meal no " + i);
+            Order order = new Order();
+            order.addMealToOrder(meal);
+            addOrderToCart(order);
+        }
+        System.out.println("Cart size: " + orders.size());
+        clearCart();
+    }
+}
---
+++ src/test/java/pl/jrkn87/junit/CartTest.java
@@ -0,0 +1,20 @@
+package pl.jrkn87.junit;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CartTest {
+
+    @Test
+    void simulateLargeOrder() {
+        //given
+        Cart cart = new Cart();
+
+        //when
+        //then
+        assertTimeout(Duration.ofMillis(100), cart::simulateLargeOrder);
+    }
+}
